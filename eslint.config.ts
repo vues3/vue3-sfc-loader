af9c073ef3818507f53ee956873f0a06c23944b7
Jerry Bruwes <jbruwes@icloud.com>
2025-03-22 12:46:43 +0200

    modified:   eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,35 +1,35 @@
-import eslint from "@eslint/js";
-import eslintPluginImportX from "eslint-plugin-import-x";
-import perfectionist from "eslint-plugin-perfectionist";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import tseslint, { configs } from "typescript-eslint";
-export default tseslint.config(
-  { ignores: ["**/dist"] },
-  {
-    rules: {
-      "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
-      "import-x/no-extraneous-dependencies": [
-        "error",
-        {
-          devDependencies: ["**/eslint.config.ts"],
-          optionalDependencies: false,
-        },
-      ],
-    },
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: { allowDefaultProject: ["eslint.config.ts"] },
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  eslint.configs.recommended,
-  eslintPluginImportX.flatConfigs.recommended,
-  eslintPluginImportX.flatConfigs.typescript,
-  configs.strictTypeChecked,
-  configs.stylisticTypeChecked,
-  perfectionist.configs["recommended-natural"],
-  eslintPluginPrettierRecommended,
-);
+// import eslint from "@eslint/js";
+// import eslintPluginImportX from "eslint-plugin-import-x";
+// import perfectionist from "eslint-plugin-perfectionist";
+// import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+// import tseslint, { configs } from "typescript-eslint";
+// export default tseslint.config(
+//   { ignores: ["**/dist"] },
+//   {
+//     rules: {
+//       "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
+//       "import-x/no-extraneous-dependencies": [
+//         "error",
+//         {
+//           devDependencies: ["**/eslint.config.ts"],
+//           optionalDependencies: false,
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     languageOptions: {
+//       parserOptions: {
+//         projectService: { allowDefaultProject: ["eslint.config.ts"] },
+//         tsconfigRootDir: import.meta.dirname,
+//       },
+//     },
+//   },
+//   eslint.configs.recommended,
+//   eslintPluginImportX.flatConfigs.recommended,
+//   eslintPluginImportX.flatConfigs.typescript,
+//   configs.strictTypeChecked,
+//   configs.stylisticTypeChecked,
+//   perfectionist.configs["recommended-natural"],
+//   eslintPluginPrettierRecommended,
+// );
